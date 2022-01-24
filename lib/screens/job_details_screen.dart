e6dce3ccb9033ab54be6a75257fd3d8d65119634
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-01-23 18:01:12 -0800

    initial work on the job details screen

---
+++ lib/screens/job_details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class JobDetailsPage extends StatelessWidget {
+  const JobDetailsPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
